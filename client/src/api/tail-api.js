001a2093ff44847a9b0ddec876c4495b7b6a53b7
shaked <shakedteler@gmail.com>
2021-05-12 11:27:00 +0300

    test

--- client/src/api/tail-api.js
+++ client/src/api/tail-api.js
@@ -3,9 +3,12 @@ import axios from 'axios';
 const api = axios.create({ baseURL: "http://localhost:4000/api"});
 
 export const insertTail = payload => api.post(`/tails`, payload);
-export const getAllTail = () => api.get(`/tails`);
+export async function getAllTail() {
+   const response = await api.get(`/tails`);
+   return response.data;
+};
 export const updateTailById = (id, payload) => api.put(`/tails/${id}`, payload);
-export const deleteTailById = id => api.delete(`/tails/${id}`);
+export  const deleteTailById = id => api.delete(`/tails/${id}`);
 export const getTailById = id => api.get(`/tails/${id}`);
 
 const tailApi = {
--- client/src/homePage/Manage-Items/Tails/tail.js
+++ client/src/homePage/Manage-Items/Tails/tail.js
@@ -2,13 +2,14 @@ import {React,useEffect,useState} from 'react';
 import tail from '../../../api/tail-api';
 const Tail = props =>{
 
-    const [Tails,setTails] = useState(tail.getAllTail);
-    let test = "";
-    Tails.then(res=>test = res.data.error);
-    let test2 = test==="Tail not found"?"Tail not found":"LOL";
+    
+    const [Tails,setTails] = useState("");
+    tail.getAllTail().then(res=>res.data?setTails(res.data):setTails(res.error));
+    
+    
     return(
     <div>
-        <h1>{test2}</h1>
+        <h1>{Tails}</h1>
     </div>
 )
 };
