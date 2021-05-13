44b6b39c9af355fd53616f2c3bd0c3cbb0e8728b
shaked <shakedteler@gmail.com>
2021-05-13 15:48:01 +0300

    nothing to say

--- client/src/homePage/Manage-Items/Tails/tail.js
+++ client/src/homePage/Manage-Items/Tails/tail.js
@@ -1,14 +1,22 @@
 import {React,useEffect,useState} from 'react';
+//axios
 import tail from '../../../api/tail-api';
+//components 
 import Button from '../../../components/button/Button';
+import ManageNav from '../../../components/managebar/ManageNav';
 import './tail.css'
 const Tail = props =>{
 
     const[newTail,setNewTail] = useState("");
     const [Tails,setTails] = useState([]);
     const[submitTail,setSubmitTail] = useState("");
+    const[_success,Set_Success] = useState(true);
     useEffect(()=>{
-        tail.getAllTail().then(res=>res.data?setTails(res.data):console.log());//TODO when error need to change to s=mongo doesnt work page
+        tail.getAllTail().then(res=>
+            {
+                res.data?setTails(res.data):console.log()
+                res.success?Set_Success(true):Set_Success(false)
+            });//TODO when error need to change to s=mongo doesnt work page
     },[]);
     useEffect(()=>{
         let flag = false;//eslint-disable-next-line
@@ -16,23 +24,29 @@ const Tail = props =>{
         !flag && submitTail.length && Number.isInteger(submitTail*1)?tail.insertTail({ID:submitTail}):console.log();
         !flag && submitTail.length && Number.isInteger(submitTail*1)?setTails([...Tails,{ID:submitTail}]):console.log();
     },[submitTail,Tails])
-    
+    const success = (
+            <div className = "TailsPage">
+               <div className = "ManageTails">
+                   מה תרצה לעשות
+                   <div className = "">
+                   <textarea className = "" 
+                   value = {newTail}
+                   onChange = {(e)=>setNewTail(e.target.value)}>
+                   </textarea>
+                   <Button  func = {()=>setSubmitTail(newTail)} name = "הוסף מספר זנב"/>
+                   </div>
+               </div>
+               <div className = "TailsTable">
+                   {!Tails.length?(<label className ="EmptyTable" >הטבלה ריקה</label>):Tails.map(tail=><label key ={tail.id} className = "Tail">{tail.ID}</label>)}
+               </div>
+         </div>
+    );
+    const error = <label>TODO MAKE ERROR PAGE FOR MONGO</label>;//TODO MAKE ERROR PAGE FOR MONGO 
     return(
-    <div className = "TailsPage">
-        <div className = "ManageTails">
-             מה תרצה לעשות
-            <div className = "">
-            <textarea className = "" 
-            value = {newTail}
-            onChange = {(e)=>setNewTail(e.target.value)}>
-            </textarea>
-            <Button  func = {()=>setSubmitTail(newTail)} name = "הוסף מספר זנב"/>
-            </div>
+        <div>
+            <ManageNav url="/tail"/>
+            {_success?success:error}
         </div>
-        <div className = "TailsTable">
-            {!Tails.length?"הטבלה ריקה":Tails.map(tail=><label key ={tail.id} className = "Tail">{tail.ID}</label>)}
-        </div>
-    </div>
-)
+    );
 };
 export default Tail;
