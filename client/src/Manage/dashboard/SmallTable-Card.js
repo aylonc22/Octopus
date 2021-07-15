ba895182df300b7c122d94f8e24d805566fcd265
aylon <aylonc1@gmail.com>
2021-07-15 21:24:34 +0300

    dasdas

--- client/src/Manage/dashboard/SmallTable-Card.js
+++ client/src/Manage/dashboard/SmallTable-Card.js
@@ -12,7 +12,6 @@ const SmallTable = props=>{
         //eslint-disable-next-line
         },[]);
    return( <div key = {props.table} className ="Card">           
-   <label className = "Table-Name"> {props.name} </label>
    <div className  = "Row"> <div className = "Header-Cell">{props.secondaryH}</div> <div className = "Header-Cell">מס"ז</div> </div>                
    {Data.slice(0,9).map((e)=><div key = {e._id} className  = "Row"> <div className = "Cell">{e.Type?e.Type:e.Location}</div> <div  className = "Cell">{e.ID}</div> </div>)}
    <div className  = "Row"> <div className = "Cell">dsasd</div> <div  className = "Cell">dsadsa</div> </div>
