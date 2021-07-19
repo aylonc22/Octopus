2e083fc46f56536ad64599bb233de9e6df632cfc
yosi <yosi@gmail.com>
2021-07-19 15:33:31 +0300

    asda

--- client/src/offlineStation/offlineStation.js
+++ client/src/offlineStation/offlineStation.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './offlineStation.css'
 import h from '../icons/haziza.png';
 
 const OfflineStation = (props) => {
+    const[focus,setFocus] = useState(undefined);
     let array = props.items.map(item => {
         return (
-            <div  key = {item.id} className = "container">
-            <label className = "stationName">{item.id}</label>
+            <div  onMouseLeave={()=>setFocus(undefined)} onMouseEnter ={()=>setFocus(item.id)} key = {item.id} className = "container">
+            <div className="card"> 
+            <div  className = "stationName">{item.id}</div>
+            <img src = {h} className = {focus===item.id?"card-image selected":"card-image"}/>
+            <div className="card-footer">
+            <div className = "SRow">
+                    <label className = "SCell">{item.message}</label>
+                        <label className = "SCell">ג</label>                        
+                    </div>
+                    <div className = "SRow">
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">ע</label>                       
+                    </div>
+                    <div className = "SRow">
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">I</label>                        
+                    </div>
+                    <div className = "SRow">                        
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">מ</label>
+                    </div>
+                    <div className = "SRow">
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">ת</label>                        
+                    </div>
+                    <div className = "SRow">                        
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">מ</label>
+                    </div>
+                    <div className = "SRow">
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">1</label>
+                    </div>
+                    <div className = "SRow">                       
+                        <label className = "SCell">1</label>
+                        <label className = "SCell">מ</label>
+                    </div> 
+            </div>
+            </div>
+            {/* <label className = "stationName">{item.id}</label>
             <div className = "top">
             <img src={h} alt="haziza"></img>
             </div>
@@ -43,7 +82,7 @@ const OfflineStation = (props) => {
                         <label className = "Cell">1</label>
                         <label className = "Cell">מ</label>
                     </div>  
-            </div>
+            </div> */}
             </div>
         )
     });
